0582c71ca3b33a8e757a539b7b4cd03263a2e3b6
Alireza Ayinmehr <alireza.darksun@gmail.com>
2019-10-24 08:07:59 +0330

    Initialize Flask Migrate

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from config import Development
 
 
@@ -7,6 +8,7 @@ app = Flask(__name__)
 app.config.from_object(Development)
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 @app.route('/')
 def index():
