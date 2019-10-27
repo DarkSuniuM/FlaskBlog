45eb90fd3a90c22ded17adc727b5982f4d3cabb2
Alireza Ayinmehr <alireza.darksun@gmail.com>
2019-10-27 03:42:57 +0330

    Add SECRET_KEY

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS', False)
+    SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 class Development(Config):
