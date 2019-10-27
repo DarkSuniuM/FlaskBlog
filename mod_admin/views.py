07ef5495fd9807ef1a615e39a55e2b18503dfb36
Alireza Ayinmehr <alireza.darksun@gmail.com>
2019-10-27 03:44:51 +0330

    Initialize admin.login view

--- mod_admin/views.py
+++ mod_admin/views.py
@@ -1,6 +1,16 @@
+from flask import session
 from . import admin
 
 
 @admin.route('/')
 def index():
     return "Hello from admin Index"
+
+
+@admin.route('/login/')
+def login():
+    session['name'] = 'Alireza'
+    # session.clear()
+    # print(session.get('name'))
+    print(session)
+    return "1"
