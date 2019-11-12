d409ac31e1f92e3bef8c4afd212e0841b338b31d
Alireza Ayinmehr <alireza.darksun@gmail.com>
2019-11-12 21:42:07 +0330

    Initialize blog views

---
+++ mod_blog/views.py
@@ -0,0 +1,15 @@
+from flask import render_template
+
+from . import blog
+from .models import Post
+
+@blog.route('/')
+def index():
+    posts = Post.query.all()
+    return render_template('blog/index.html', posts=posts)
+
+
+@blog.route('/<string:slug>')
+def single_post(slug):
+    post = Post.query.filter(Post.slug == slug).first()
+    return post.title
