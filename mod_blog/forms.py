0c640cace5921810c8e096ad8be31140c6ef8001
Alireza Ayinmehr <alireza.darksun@gmail.com>
2019-12-02 10:29:42 +0330

    Replace {Create,Modify}PostForm with PostForm and Add Categories to PostForm

--- mod_blog/forms.py
+++ mod_blog/forms.py
@@ -1,20 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import TextField, TextAreaField
 from wtforms.validators import DataRequired
+from utils.forms import MultipleCheckboxField
 
 
-class CreatePostForm(FlaskForm):
+class PostForm(FlaskForm):
     title = TextField(validators=[DataRequired()])
     summary = TextAreaField()
     content = TextAreaField(validators=[DataRequired()])
     slug = TextField(validators=[DataRequired()])
-
-
-class ModifyPostForm(FlaskForm):
-    title = TextField(validators=[DataRequired()])
-    summary = TextAreaField()
-    content = TextAreaField(validators=[DataRequired()])
-    slug = TextField(validators=[DataRequired()])
+    categories = MultipleCheckboxField(coerce=int)
 
 
 class CategoryForm(FlaskForm):
