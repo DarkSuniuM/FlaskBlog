b1833a27e634ba75fd2892711fb42f08fbc8bcd3
Alireza Ayinmehr <alireza.darksun@gmail.com>
2019-12-06 04:24:40 +0330

    Register mod_uploads

--- app.py
+++ app.py
@@ -15,7 +15,9 @@ from views import index
 from mod_admin import admin
 from mod_users import users
 from mod_blog import blog
+from mod_uploads import uploads
 
 app.register_blueprint(admin)
 app.register_blueprint(users)
 app.register_blueprint(blog)
+app.register_blueprint(uploads)
