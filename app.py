0402d6f760a24007775a067071e843c17f023daa
Alireza Ayinmehr <alireza.darksun@gmail.com>
2019-12-09 21:13:37 +0330

    Initialize flask-Mail

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_mail import Mail
 from config import Development
 
 
@@ -9,6 +10,7 @@ app.config.from_object(Development)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+mail = Mail(app)
 
 from views import index
 
