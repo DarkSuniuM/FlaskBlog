b089b7e9a9ad9159c226d2469f0419e0a1a38224
Alireza Ayinmehr <alireza.darksun@gmail.com>
2019-12-09 21:13:18 +0330

    Add FlaskMail Config Keys

--- config.py
+++ config.py
@@ -5,6 +5,9 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS', False)
     SECRET_KEY = os.getenv('SECRET_KEY')
+    MAIL_SERVER = os.getenv('MAIL_SERVER')
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
 
 
 class Development(Config):
