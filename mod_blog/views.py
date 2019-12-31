2cb635220e04f5b210e2bab1925bced262841e40
Alireza Ayinmehr <alireza.darksun@gmail.com>
2019-12-31 15:45:42 +0330

    Add Pagination to blog.index

--- mod_blog/views.py
+++ mod_blog/views.py
@@ -8,8 +8,9 @@ from .forms import SearchForm
 
 @blog.route('/')
 def index():
+    page = request.args.get('p', default=1, type=int)
     search_form = SearchForm()
-    posts = Post.query.all()
+    posts = Post.query.paginate(page, 5)
     return render_template('blog/index.html', posts=posts, search_form=search_form)
 
 
