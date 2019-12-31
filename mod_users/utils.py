c44cac83aa3cb3367e7ac4215864f7d997824266
Alireza Ayinmehr <alireza.darksun@gmail.com>
2019-12-31 15:46:36 +0330

    Add URL to the mail body

--- mod_users/utils.py
+++ mod_users/utils.py
@@ -1,4 +1,5 @@
 import random
+from flask import url_for
 from app import redis, mail
 
 
@@ -20,8 +21,10 @@ def delete_from_redis(user, mode):
 
 
 def send_signup_message(user, token):
+    url = url_for('users.confirm_registeration', email=user.email, token=token, _external=True)
+
     sender = 'flasktuts@ayinmehr.ir'
     recipients = [user.email]
     subject = 'Flask Blog - Registeration Confirm'
-    body = f'Hello,<br>Here is your token: {token}.'
+    body = f'Hello,<br>Open this URL: {url}.'
     mail.send_message(sender=sender, recipients=recipients, subject=subject, html=body)
